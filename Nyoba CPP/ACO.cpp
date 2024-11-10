d2b0aba40dabec60bd216783864dbcf62369949c
Gdankz <gabrielbayu03@gmail.com>
2024-11-10 22:01:29 +0700

    First Commit

---
+++ Nyoba CPP/ACO.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <random>
+
+using namespace std;
+
+const int NUM_ANTS = 4;
+const int MAX_ITERATIONS = 10;
+const double ALPHA = 1.0;
+const double BETA = 1.0;
+const double RHO = 0.15;
+const double INITIAL_PHEROMONE = 1.0;
+
